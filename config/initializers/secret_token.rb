b10ff8168433e85c39f61ab61475d53602b2d5dd
Eric Scott <eric@splittr.com>
2012-02-26 21:06:19 -0800

    initial commit with the rails stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeatFeed::Application.config.secret_token = '671c58c2e59bb2505d7f01a492c497b6c84351aa02953cb892dedb45328212d10159ff5d24f2372ba512b87bcb2f69f2d1d01857bad03f56f882cfe1c9546a6d'
