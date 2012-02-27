d88b1d4f2a15a367d89a5ceffb021ebf51449b73
Arielle Zuckerberg <arielle.zuckerberg@wildfireapp.com>
2012-02-26 23:12:56 -0800

    added fetchBeats script to home

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120227051228) do
+ActiveRecord::Schema.define(:version => 20120227054059) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
